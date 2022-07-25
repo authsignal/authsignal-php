f5dcb20f4ba2e8a49b844c289564d4edb4c2e40c
justinsoong <justinsoong@gmail.com>
2022-07-25 15:34:51 +1200

    0.1.1

--- test/AuthsignalTest.php
+++ test/AuthsignalTest.php
@@ -3,12 +3,24 @@
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
+use donatj\MockWebServer\MockWebServer;
+use donatj\MockWebServer\Response;
+
 class AuthsignalTest extends PHPUnit\Framework\TestCase {
+    /** @var MockWebServer */
+	protected static $server;
 
     public static function setUpBeforeClass(): void {
         Authsignal::setApiKey('secret');
-        Authsignal::setApiHostname('http://localhost:8080');
+        self::$server = new MockWebServer;
+		self::$server->start();
+
+        Authsignal::setApiHostname(self::$server->getServerRoot());
     }
+
+    static function tearDownAfterClass(): void {
+		self::$server->stop();
+	}
 
     public function testSetApiKey()
     {
@@ -16,6 +28,13 @@ class AuthsignalTest extends PHPUnit\Framework\TestCase {
     }
 
     public function testTrackAction() {
+        // Mock response
+        $mockedResponse = array("state" => "ALLOW",
+              "idempotencyKey" => "5924a649-b5d3-4baf-a4ab-4b812dde97a0",
+              "ruleIds" => []);
+
+        self::$server->setResponseOfPath('/v1/users/123%3Atest/actions/signIn', new Response(json_encode($mockedResponse)));
+
         $payload = array(
             "redirectUrl" => "https://www.yourapp.com/back_to_your_app",
             "email" => "test@email",
@@ -31,25 +50,69 @@ class AuthsignalTest extends PHPUnit\Framework\TestCase {
         $response = Authsignal::trackAction(userId: "123:test",
                                             actionCode: "signIn",
                                             payload: $payload);
+
+        $this->assertEquals($response["state"], "ALLOW");
+        $this->assertEquals($response["idempotencyKey"], $mockedResponse["idempotencyKey"]);
     }
 
     public function testgetAction() {
+        // Mock response
+        $mockedResponse = array("state" => "ALLOW",
+              "idempotencyKey" => "5924a649-b5d3-4baf-a4ab-4b812dde97a0",
+              "stateUpdatedAt" => "2022-07-25T03:19:00.316Z",
+              "createdAt" => "2022-07-25T03:19:00.316Z",
+              "ruleIds" => []);
+
+        self::$server->setResponseOfPath("/v1/users/123%3Atest/actions/signIn/5924a649-b5d3-4baf-a4ab-4b812dde97a04", new Response(json_encode($mockedResponse)));
+
         $response = Authsignal::getAction(userId: "123:test",
                                             actionCode: "signIn",
-                                            idempotencyKey: "2320ce18-91be-47a8-9bbf-eec642807c34");
+                                            idempotencyKey: "5924a649-b5d3-4baf-a4ab-4b812dde97a04");
+
+        $this->assertEquals($response["state"], "ALLOW");
+        $this->assertEquals($response["idempotencyKey"], $mockedResponse["idempotencyKey"]);
+        $this->assertEquals($response["stateUpdatedAt"], $mockedResponse["stateUpdatedAt"]);
     }
 
     public function testgetUser() {
+        $mockedResponse = array("isEnrolled" => false,
+              "accessToken" => "xxxx",
+              "url" => "wwwww");
+
+        self::$server->setResponseOfPath("/v1/users/123%3Atest", new Response(json_encode($mockedResponse)));
         $response = Authsignal::getUser(userId: "123:test", redirectUrl: "https://www.example.com/");
+        
+        $this->assertEquals($response["isEnrolled"], $mockedResponse["isEnrolled"]);
+        $this->assertEquals($response["url"], $mockedResponse["url"]);
     }
 
     public function testIdentify() {
-        $response = Authsignal::getUser(userId: "123:test", redirectUrl: "https://www.example.com/");
+        $mockedResponse = array("userId" => "123:test",
+              "email" => "test@test.com");
+
+        self::$server->setResponseOfPath("/v1/users/123%3Atest", new Response(json_encode($mockedResponse)));
+        $response = Authsignal::identify(userId: "123:test", user: array("email" => "test@test.com"));
+
+        $this->assertEquals($response["userId"], $mockedResponse["userId"]);
+        $this->assertEquals($response["email"], $mockedResponse["email"]);
     }
 
     public function testEnrolAuthenticator() {
+        $mockedResponse = array(
+            "authenticator" => array(
+                "userAuthenticatorId" => "9b2cfd40-7df2-4658-852d-a0c3456e5a2e",
+                "authenticatorType" => "OOB",
+                "isDefault" => true,
+                "phoneNumber"=> "+6427000000",
+                "createdAt"=> "2022-07-25T03:31:36.219Z",
+                "oobChannel"=> "SMS"));
+
+        self::$server->setResponseOfPath("/v1/users/123%3Atest/authenticators", new Response(json_encode($mockedResponse)));
+
         $response = Authsignal::enrolAuthenticator(userId: "123:test",
                                                    authenticator: array("oobChannel" => "SMS"
-                                                                ,"phoneNumber" => "+64273330770"));
+                                                                ,"phoneNumber" => "+6427000000"));
+        
+        $this->assertEquals($response["authenticator"]["userAuthenticatorId"], $mockedResponse["authenticator"]["userAuthenticatorId"]);
     }
 }
